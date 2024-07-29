8b24ab1905a566214de0805f69df58e0d382aa92
Christopher Widling <christopher.widling@gmail.com>
2024-07-30 01:29:06 +0200

    refactor and comments

--- db/utils.py
+++ db/utils.py
@@ -1,51 +1,45 @@
-import hashlib
-import os
-import datetime
-import re
+
+import datetime, os
 from sqlalchemy.orm import Session
-from sqlalchemy import exists
+from config import Config
 from .models import Authentification, WebsitePermissions, WebApiSession
 
-def generate_salt(length=8):
-    return os.urandom(length).hex()
+keycounter = 0
 
-def hash_password(password, salt):
-    first_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
-    hasher = hashlib.sha256()
-    hasher.update((first_hash + salt).encode('utf-8'))
-    hashcode = hasher.hexdigest()
-    return f"$SHA${salt}${hashcode}"
+def generate_hex(length=32):
+    return os.urandom((length+1)/2).hex()[length:]
     
 def get_user_by_username(db: Session, username: str):
     return db.query(Authentification).filter(Authentification.username == username).first()
 
-def verify_password(stored_password: str, provided_password: str):
-    if not stored_password.startswith("$SHA$"):
-        return False
-    salt = stored_password.split('$')[2]
-    return stored_password == hash_password(provided_password, salt)
-
 def gen_api_key(db: Session, username: str):
-    api_key = "API_" + os.urandom(22).hex()
+    """
+        Generates and return a new API key for the given user.
+    """
+    api_key = f"{keycounter:03d}_{generate_hex(28)}"
        
-    # Berechtigungsstufe des Benutzers abrufen
+    # Get users permission level from database
     permission = db.query(WebsitePermissions).filter(WebsitePermissions.username == username).first()
-    perm_level = 0
-
+    
     if permission:
         perm_level = permission.perm_level
+    else:
+        # Set level to 0 if no entry
+        perm_level = 0
     
-    # Berechne den Zeitpunkt, der 10 Minuten in der Zukunft liegt
-    valid_until = datetime.datetime.now() + datetime.timedelta(minutes=10)
+    # Calculate validity duration
+    valid_until = datetime.datetime.now() + datetime.timedelta(minutes=Config.KEY_VALID_DURATION)
     
-    # API-Schlüssel zusammen mit username, valid_until und perm_level in webapi_session abspeichern
+    # Save API key with username, valid_until and perm_level in webapi_session
     new_session = WebApiSession(username=username, api_key=api_key, valid_until=valid_until, perm_level=perm_level)
     db.add(new_session)
     db.commit()
-    
     return api_key
 
 def invalidate_api_key(db: Session, api_key: str):
+    """
+        Invalidates the given API key.
+    """
     session = db.query(WebApiSession).filter(WebApiSession.api_key == api_key).first()
     if session:
         db.delete(session)
@@ -55,16 +49,25 @@ def invalidate_api_key(db: Session, api_key: str):
         return "API key not found."
     
 def is_api_key_valid(db: Session, api_key: str):
-    # Überprüfen, ob der API-Schlüssel gültig ist (valid_until in der Zukunft)
+    """
+        Checks if API key is still valid.
+    """
     session = db.query(WebApiSession).filter(WebApiSession.api_key == api_key).first()
     return session and session.valid_until > datetime.datetime.now()
     
 def is_perm_level_sufficient(db: Session, api_key: str, required_level: int):
-    # Überprüfen, ob die Berechtigungsstufe des API-Schlüssels ausreichend ist
+    """
+        Checks if API key owners permission level is at least the required level.
+    """
     session = db.query(WebApiSession).filter(WebApiSession.api_key == api_key).first()
     return session and session.perm_level >= required_level
     
 def is_action_permitted(db: Session, username: str, api_key: str, required_level: int):
+    """
+        Checks if API key owner is permitted to perform an action.
+        Unlike is_perm_level_sufficient it also checks if key is used by the rightful
+        owner which is safer.
+    """
     session = db.query(WebApiSession).filter(WebApiSession.api_key == api_key).first()
     return (session and session.perm_level >= required_level and 
         session.valid_until > datetime.datetime.now() and session.username==username)
