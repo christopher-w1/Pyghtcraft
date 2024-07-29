1b4fb6ca4ac857e601e093c6e80ac0b91deaf532
Christopher Widling <christopher.widling@gmail.com>
2024-07-30 01:28:36 +0200

    refactor

--- db/models.py
+++ db/models.py
@@ -1,20 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, BigInteger, DateTime
 from . import Base
 
-class Authentification(Base):
-    __tablename__ = 'authentifications'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    username = Column(String(255), unique=True, nullable=False)
-    realname = Column(String(255), unique=True, nullable=False)
-    password = Column(String(255), nullable=False)
-    x = Column(Float, nullable=False, default=0.5)
-    y = Column(Float, nullable=False, default=65.0)
-    z = Column(Float, nullable=False, default=0.5)
-    regdate = Column(BigInteger, nullable=False, default=0.5)
-    regip = Column(String(40), nullable=False, default="0.0.0.0")
-    email = Column(String(255), nullable=False)
-
-
 class WebApiSession(Base):
     __tablename__ = 'webapi_session'
     username = Column(String(255), primary_key=True, nullable=False)
